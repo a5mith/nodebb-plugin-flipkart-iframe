8ccc44d4639ac05cc7354cc9a29af2a0f81ef823
a5mith <aaronsmith91@sky.com>
2014-10-03 03:53:34 +0100

    ability to add height

--- library.js
+++ library.js
@@ -2,11 +2,11 @@
     "use strict";
 
     var flipkart = {},
-        embed = '<iframe class="flipkart" src="http://www.flipkart.com/affiliate/displayWidget?affrid=$1" frameborder=0></iframe>';
+        embed = '<iframe class="flipkart" width="100%" height="$2" src="http://www.flipkart.com/affiliate/displayWidget?affrid=$1" frameborder=0></iframe>';
 
 
     flipkart.parse = function(postContent, callback) {
-        postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)flipkart\.com\/affiliate\/displayWidget\?(?:[^"]*&)?affrid=([\w\-_]+).+<\/a>/ig, embed);
+        postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)flipkart\.com\/affiliate\/displayWidget\?(?:[^"]*&)?affrid=([\w\-_]+)(?:&[^"#]*)?(?:#(\d+))?.+<\/a>/ig, embed);
         callback(null, postContent);
     };
 
